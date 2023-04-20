c4e483e906269726a9b8e237594300f25ec7842a
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-04-20 15:20:13 -0500

    shift hour changed

--- src/helpers/formattedDate.ts
+++ src/helpers/formattedDate.ts
@@ -1,24 +1,24 @@
 export const formattedDate = () => {
-  const currentDate = new Date()
-  const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-  //@ts-ignore
-  const formattedDate = currentDate.toLocaleDateString('en-GB', options);
-  return formattedDate.replace(/\//g, '-')
+    const currentDate = new Date()
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    //@ts-ignore
+    const formattedDate = currentDate.toLocaleDateString('en-GB', options);
+    return formattedDate.replace(/\//g, '-')
 }
 
 export const isDayShift = () => {
     const now = new Date(); // obtener la hora actual
     const hours = now.getHours(); // obtener la hora actual
     const minutes = now.getMinutes(); // obtener los minutos actuales
-  
-    if (hours < 15) {
-      // si la hora es antes de las 3pm, es cierto
-      return true;
-    } else if (hours === 15 && minutes <= 30) {
-      // si la hora es 3pm y los minutos son 30 o menos, es cierto
-      return true;
+
+    if (hours < 16) {
+        // si la hora es antes de las 3pm, es cierto
+        return true;
+    } else if (hours === 16 && minutes <= 30) {
+        // si la hora es 3pm y los minutos son 30 o menos, es cierto
+        return true;
     } else {
-      // de lo contrario, es falso
-      return false;
+        // de lo contrario, es falso
+        return false;
     }
-  }
+}
